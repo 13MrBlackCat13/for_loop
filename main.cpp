005b4a34f5133eb75f58ff03301fc47c39ecb4d8
Михаил <69348201+13MrBlackCat13@users.noreply.github.com>
2021-09-11 19:26:47 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int distance;
+
+    for(distance = 0; distance < 200;){
+    distance = 40 + distance;
+    cout << distance << endl;
+    }
+    return 0;
+}
